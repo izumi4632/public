b69689c6e41b611c4114cbccfff616d2516cbbe1
tundokubabel <ujiu2kyr@gmail.com>
2020-05-29 16:30:18 +0900

    commit

--- abc/d/168d(深さ優先探索、深さ記録、一方通行、構成).jl
+++ abc/d/168d(深さ優先探索、深さ記録、一方通行、構成).jl
@@ -3,26 +3,25 @@ pI(x)=parse(Int,x)
 pM(x::Array{SubString{String},1})=map(pI,x)
 function main()
   n,m=pM(split(readline()))
-  g=[Int[] for i=1:n]
+  graph=[Int[] for i=1:n]
   for i=1:m;
     a,b=pM(split(readline()))
-    push!(g[a],b)
-    push!(g[b],a)
+    push!(graph[a],b)
+    push!(graph[b],a)
   end
   que=Int[1]
-  depth=zeros(Int,n)
-  depth[1]=1
+  mezirusi=zeros(Int,n)
+  mezirusi[1]=1
   while !isempty(que)
     look=popfirst!(que)
-    for next=g[look]
-      if depth[next]==0
-        depth[next]=look+1
-        push!(que,next)
-      end
+    for next=graph[look]
+      if mezirusi[next]!=0;continue;end
+      mezirusi[next]=look+1
+      push!(que,next)
     end
   end
   println("Yes")
-  for i=depth[2:end];println(i-1);end
+  for i=mezirusi[2:end];println(i-1);end
 end
 main()
 if isdefined(Base, :mystdin);close(mystdin);end
