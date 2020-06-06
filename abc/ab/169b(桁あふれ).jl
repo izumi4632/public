ca228786332f2cdc9d73cd1bc60b82f0b39058c8
tundokubabel <ujiu2kyr@gmail.com>
2020-06-07 08:26:09 +0900

    Subject < 50 chars
    Body in detail

--- abc/ab/169b(桁あふれ).jl
+++ abc/ab/169b(桁あふれ).jl
@@ -2,20 +2,21 @@ if isfile("mystdin.txt");mystdin=open("mystdin.txt","r");redirect_stdin(mystdin)
 pI(x)=parse(Int,x)
 pM(x::Array{SubString{String},1})=map(pI,x)
 function main()
+  ans::BigInt=1
   n=pI(readline())
-  ans=0
-  l=[1]
-  if n==1;print(0);return 0;end
-  while n!=1
-    for i=l[end]+1:n
-      if n%i!=0; continue;end
-      ans+=1
-      push!(l,i)
-      n=div(n,i)
-      break
+  l=pM(split(readline()))
+  if 0 in l
+    print(0)
+    return 0
+  end
+  for i in l
+    ans*=i
+    if ans>10^18
+      print(-1)
+      return 0
     end
   end
-  print(ans)
+  println(ans)
 end
 main()
 if isdefined(Base, :mystdin);close(mystdin);end
